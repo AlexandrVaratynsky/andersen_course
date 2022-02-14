ffa84ea01fa002967ece1e13ca0d764f51fba42c
AlexandrV <varatynski.a@gmail.com>
2022-02-15 02:08:12 +0300

    added delete(), get() and tests

--- src/main/java/com/andersen/pre_intensive/LinkedListImplementation.java
+++ src/main/java/com/andersen/pre_intensive/LinkedListImplementation.java
@@ -28,19 +28,61 @@ public class LinkedListImplementation<T> implements MyLinkedList<T> {
 
 	}
 
+	Node<T> findListElementByIndex(int index) {
+
+		if (index < (size / 2)) {
+			Node<T> node = firstNode;
+			for (int i = 0; i < index; i++)
+				node = node.next;
+			return node;
+		} else {
+			Node<T> node = lastNode;
+			for (int i = size - 1; i > index; i--)
+				node = node.prev;
+			return node;
+		}
+	}
+
 	@Override
 	public boolean delete(int index) {
-		return false;
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException("index: " + index + ", size: " + size);
+		}
+
+		Node<T> findedListElement = findListElementByIndex(index);
+		final Node<T> next = findedListElement.next;
+		final Node<T> prev = findedListElement.prev;
+
+		if (prev == null) {
+			firstNode = next;
+		} else {
+			prev.next = next;
+			findedListElement.prev = null;
+		}
+
+		if (next == null) {
+			lastNode = prev;
+		} else {
+			next.prev = prev;
+			findedListElement.next = null;
+		}
+
+		findedListElement.value = null;
+		size--;
+		return true;
 	}
 
 	@Override
 	public T get(int index) {
-		return null;
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+		}
+		return findListElementByIndex(index).value;
 	}
 
 	@Override
 	public int size() {
-		return 0;
+		return size;
 	}
 
 	private static class Node<T> {
--- src/test/java/com/andersen/pre_intensive/LinkedListImplementationTest.java
+++ src/test/java/com/andersen/pre_intensive/LinkedListImplementationTest.java
@@ -1,5 +1,6 @@
 package com.andersen.pre_intensive;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -22,12 +23,48 @@ public class LinkedListImplementationTest {
     public void concat() {
     }
 
-    @Test
-    public void delete() {
+    LinkedListImplementation<String> list=new LinkedListImplementation<>();
+
+    @Before
+    public void createTestList() {
+
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
     }
 
     @Test
-    public void get() {
+    public void deleteElementByIndexMiddle() {
+        list.delete(0);
+        assertEquals(list.size(), 3);
+    }
+
+    @Test
+    public void deleteElementByIndexFirst() {
+        list.delete(2);
+        assertEquals(list.size(), 3);
+    }
+
+    @Test
+    public void deleteElementByIndexLast() {
+        list.delete(list.size()-1);
+        assertEquals(list.size(), 3);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void deleteElementByWrongIndex() {
+        list.delete(-1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void getElementByWrongIndex() {
+        list.get(-1);
+    }
+
+    @Test
+    public void getElementByIndex() {
+        assertEquals(list.get(0), "1");
     }
 
     @Test
