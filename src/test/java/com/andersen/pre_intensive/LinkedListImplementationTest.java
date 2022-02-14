c8a07687fded7e29f7ee026dd67fcf64e92af750
home <home@DESKTOP-N0GUOBR>
2022-02-15 02:35:57 +0300

    Тест size Тест add заглушка

--- src/test/java/com/andersen/pre_intensive/LinkedListImplementationTest.java
+++ src/test/java/com/andersen/pre_intensive/LinkedListImplementationTest.java
@@ -4,10 +4,26 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+
+import org.junit.Assert;
+
 public class LinkedListImplementationTest {
 
     @Test
     public void add() {
+    	
+    	LinkedListImplementation<Integer> linkList = new LinkedListImplementation<Integer>();
+		
+		linkList.add(0);
+		linkList.add(1);
+		linkList.add(2);
+		
+		
+		
+		
+		
+    	Assert.assertEquals(linkList, linkList);
     }
 
     @Test
@@ -32,5 +48,15 @@ public class LinkedListImplementationTest {
 
     @Test
     public void size() {
+    	
+    	LinkedListImplementation<Integer> linkList = new LinkedListImplementation<Integer>();
+		
+		linkList.add(0);
+		linkList.add(1);
+		linkList.add(2);
+		
+		int size = linkList.size();
+		Assert.assertEquals(3, size);
+    	
     }
 }
