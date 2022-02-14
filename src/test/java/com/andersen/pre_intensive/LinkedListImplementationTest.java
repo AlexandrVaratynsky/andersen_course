539de6363edf47925b5612cb3e935373bd6a586e
Marat Shigapov <mshigapov13@gmail.com>
2022-02-14 21:22:08 +0300

    changes due to maven adding

---
+++ src/test/java/com/andersen/pre_intensive/LinkedListImplementationTest.java
@@ -0,0 +1,36 @@
+package com.andersen.pre_intensive;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LinkedListImplementationTest {
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void testAdd() {
+    }
+
+    @Test
+    public void sort() {
+    }
+
+    @Test
+    public void concat() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void size() {
+    }
+}
