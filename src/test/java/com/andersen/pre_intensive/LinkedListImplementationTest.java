2f963ad56af94fa6a6fe0e4668a505421d6ba814
Marat Shigapov <mshigapov13@gmail.com>
2022-02-15 03:29:12 +0300

    added test for addAll()

--- src/test/java/com/andersen/pre_intensive/LinkedListImplementationTest.java
+++ src/test/java/com/andersen/pre_intensive/LinkedListImplementationTest.java
@@ -2,7 +2,10 @@ package com.andersen.pre_intensive;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class LinkedListImplementationTest {
 
@@ -32,5 +35,17 @@ public class LinkedListImplementationTest {
 
     @Test
     public void size() {
+    }
+
+    @Test
+    public void addAll() {
+        String[] array = {"Hello", "World", "Test"};
+        LinkedListImplementation<String> list = new LinkedListImplementation<>(array);
+
+        assertThat(list.getArrayOfValues())
+                .hasSize(array.length)
+                .containsAll(Arrays.asList(array))
+                .doesNotContain("ERROR")
+                .doesNotContain(666);
     }
 }
